c3a2c3a7afb789e903e079a9402e4d0894df11f6
Bruce <tseng1985@gmail.com>
2018-05-08 16:32:35 +0800

    add views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/post', function(req, res, next) {
+  res.render('post');
+});
+
+router.get('/dashboard/archives', function(req, res, next) {
+  res.render('dashboard/archives');
+});
+
+router.get('/dashboard/article', function(req, res, next) {
+  res.render('dashboard/article');
+});
+
+router.get('/dashboard/categories', function(req, res, next) {
+  res.render('dashboard/categories');
+});
+
+router.get('/dashboard/signup', function(req, res, next) {
+  res.render('dashboard/signup');
+});
+
 module.exports = router;
