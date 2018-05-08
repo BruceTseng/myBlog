255180c758bfe60778635d15b334508f31332471
Bruce <tseng1985@gmail.com>
2018-05-08 17:27:09 +0800

    delete unused file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var firebaseAdminDb = require('../connection/firebase_admin');
+
+const ref = firebaseAdminDb.ref('any');
+ref.once('value', function(snapshot) {
+  console.log(snapshot.val());
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
