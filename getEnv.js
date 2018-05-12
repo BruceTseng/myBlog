4a1568b1596e78bf6c94f86b3ad6a00a8f91830a
Bruce <tseng1985@gmail.com>
2018-05-12 14:31:04 +0800

    update env.js

--- getEnv.js
+++ getEnv.js
@@ -2,11 +2,11 @@
 
 const fs = require('fs')
 
-const dotEnvExists = fs.existsSync('.env')
-if (dotEnvExists) {
-  console.log('getEnv.js: .env exists, probably running on development environment')
-  process.exit()
-}
+// const dotEnvExists = fs.existsSync('.env1')
+// if (dotEnvExists) {
+//   console.log(dotEnvExists, 'getEnv.js: .env exists, probably running on development environment')
+//   process.exit()
+// }
 
 // On Google Cloud Platform authentication is handled for us
 const gcs = require('@google-cloud/storage')()
@@ -14,7 +14,7 @@ const gcs = require('@google-cloud/storage')()
 const bucketName = `${process.env.GCLOUD_PROJECT}`
 console.log(`Downloading .env from bucket "${bucketName}"`)
 gcs
-  .bucket(bucketName)
+  .bucket('envvars_my_blog')
   .file('.env')
   .download({ destination: '.env' })
   .then(() => {
